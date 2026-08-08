7791b18a23cdb1cb3d9d134cf1bcebe17b4783a7
UENO, M. <eunosium-1128@live.jp>
2026-08-09 02:27:27 +0900

    Fix sed command and update includes in build.bat
    Updated sed command to modify CMakeLists.txt and added include for array in schema-test.cpp.

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,12 +2,12 @@
 
 set PATH=%SRC_DIR%\build\bin;%PATH%
 
-sed -i.bak 's#CPMAddPackage("gh:catchorg/Catch2@3.4.0")#find_package(Catch2 REQUIRED)#' test/CMakeLists.txt || exit /b 1
+sed -i 's#CPMAddPackage("gh:catchorg/Catch2@3.4.0")#find_package(Catch2 REQUIRED)#' test/CMakeLists.txt || exit /b 1
+sed -i '/#include <cassert>/a #include <array>' test/schema-test.cpp
 
 cmake -S . -B build -G Ninja ^
     %CMAKE_ARGS% ^
     -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
-    -DCMAKE_CXX_SCAN_FOR_MODULES=OFF ^
     -DBUILD_TESTING=ON ^
     -DBUILD_SHARED_LIBS=ON ^
     || exit /b 1
