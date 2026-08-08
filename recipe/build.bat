8e231b6b5e618352d940799327e44a41d555494e
UENO, M. <eunosium-1128@live.jp>
2026-08-09 02:27:46 +0900

    Fix sed command to include error handling

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,7 +3,7 @@
 set PATH=%SRC_DIR%\build\bin;%PATH%
 
 sed -i 's#CPMAddPackage("gh:catchorg/Catch2@3.4.0")#find_package(Catch2 REQUIRED)#' test/CMakeLists.txt || exit /b 1
-sed -i '/#include <cassert>/a #include <array>' test/schema-test.cpp
+sed -i '/#include <cassert>/a #include <array>' test/schema-test.cpp || exit /b 1
 
 cmake -S . -B build -G Ninja ^
     %CMAKE_ARGS% ^
