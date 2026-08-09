4684eab45a7665a83acf99e574a195f65e94616d
UENO, M. <eunosium-1128@live.jp>
2026-08-09 05:55:17 +0000

    Update build script and requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,11 @@
-set -exo pipefail
+
+
+cmake -S . -B build -G Ninja ^
+    %CMAKE_ARGS% ^
+    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+    -DBUILD_TESTING=ON ^
+    -DBUILD_SHARED_LIBS=ON ^
+    || exit /b 1
 
 if [[ ${target_platform} == "osx-"* ]]; then
     # std::chrono::current_zone (C++20 tzdb) requires libc++ 19+ built with
@@ -8,13 +15,19 @@ if [[ ${target_platform} == "osx-"* ]]; then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -fexperimental-library"
 fi
 
-if [[ ${build_platform} != ${target_platform} ]]; then
-    extra_cmake_args="-DTEST_STD_CHRONO_FROM_STREAM_R=ON -DCMAKE_CROSSCOMPILING=ON"
+EXTRA_CMAKE_ARGS=
+if [[ ${target_platform} == "win-"* ]]; then
+    export PATH=${SRC_DIR}/build:${PATH}
+    EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON"
 fi
 
-cmake -S . -B build -G Ninja ${CMAKE_ARGS} -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON ${extra_cmake_args}
+if [[ ${build_platform} != ${target_platform} ]]; then
+    EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DTEST_STD_CHRONO_FROM_STREAM_R=ON -DCMAKE_CROSSCOMPILING=ON"
+fi
+
+cmake -S . -B build -G Ninja ${CMAKE_ARGS} -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON ${EXTRA_CMAKE_ARGS}
 cmake --build build --parallel ${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
     ctest -V --test-dir build
 fi
-cmake --install build
+cmake --install build --parallel ${CPU_COUNT}
