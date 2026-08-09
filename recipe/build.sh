488147ae4ab24923cc5cb356495253dc33c006f5
UENO, M. <eunosium-1128@live.jp>
2026-08-09 15:28:34 +0900

    Update build.sh to modify PATH for Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ fi
 
 EXTRA_CMAKE_ARGS=
 if [[ ${target_platform} == "win-"* ]]; then
-    export LD_LIBRARY_PATH="${SRC_DIR}/build:${SRC_DIR}/build/bin:${LD_LIBRARY_PATH}"
+    export PATH="${SRC_DIR}/build:${PATH}"
     EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON"
 fi
 
